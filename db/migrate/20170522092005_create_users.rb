068f9af71704ffbd02f4275a208ebbf5e3f00b7c
Daniel Nacenta <dnacenta2@gmail.com>
2017-05-31 09:40:10 +0200

    Add units

--- db/migrate/20170522092005_create_users.rb
+++ db/migrate/20170522092005_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.belongs_to :unit, index: true
       t.string :name
       t.string :email
       t.string :last_name
