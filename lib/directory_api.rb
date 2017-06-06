47382edba5d3d44d652826105ddca350a1f92e6d
Daniel Nacenta <dnacenta2@gmail.com>
2017-06-06 09:43:23 +0200

    Fix directory and uweb integration

--- lib/directory_api.rb
+++ lib/directory_api.rb
@@ -1,37 +1,46 @@
 class DirectoryApi
+  #deactivated for testing
+  #integrated with uweb
+  #see instructions below to make it work
+
+  def initialize(data = {})
+    @user_params = { login: data[:login] }.with_indifferent_access
+    @errors      = []
+  end
 
   def client
     @directory_client ||= Savon.client(wsdl: Rails.application.secrets.directorio_wsdl,encoding: 'ISO-8859-1', raise_errors: true)
   end
 
-  def get_units(cod_organico)
-    message_params = { aplicacion: Rails.application.secrets.directorio_application_key, codOrganico: cod_organico}
-    request(:buscar_dependencias, message_params)
-  end
-
-  def hierarchical_search(id_unidad, query_type)
-    query_type_int = {only_sons: 1, all_down: 2, all_up: 3}[query_type]
-    message_params = { aplicacion: Rails.application.secrets.directorio_application_key,
-                       idUnidad: id_unidad, tipoConsulta: query_type_int}
-    request(:busqueda_jerarquica, message_params)
-  end
-
-  def employees_data(params)
-    params[:i_tipoConsulta] = {only_unit: 1, all_down: 2}[params[:i_tipoConsulta]] if params[:i_tipoConsulta].present?
-    message_params = {aplicacion: Rails.application.secrets.directorio_application_key}
-    message_params.merge!(params)
-    request(:datos_empleados, message_params)
-  end
-
-  def request(operation, message_params)
-    response = client.call(operation.to_sym, message: message_params).body
-    parsed_response = parser.parse(response[(operation.to_s + '_response').to_sym][(operation.to_s + '_return').to_sym])
-  rescue Savon::Error => e
-    puts "#{operation} - #{message_params} - Error Savon: #{e}"
-    false
+  def directory
+    #you can integrate directory with uweb adding these lines into uweb authenticator + get_d_user_data + client(rename it to d_client and change also d_client in d_response instead of client)
+    d_operation         = :datos_empleados
+    d_response          = client.call(d_operation.to_sym, message: { aplicacion: Rails.application.secrets.directorio_application_key, i_pernr: nil, i_nif: nil, i_ayre: @user_params[:login]}).body
+    d_parsed_response   = parser.parse(d_response[(d_operation.to_s + '_response').to_sym][(d_operation.to_s + '_return').to_sym])
+    self.uweb_user_data = Hash.deep_strip! get_d_user_data(d_parsed_response)
+    #you can add rescue for getting errors
   end
 
   private
+
+  def get_d_user_data(d_parsed_response)
+    #add all these attributes in the user table, they are just for checking
+    return unless d_parsed_response == nil
+    user_data = d_parsed_response.fetch('EMPLEADOS_ACTIVOS').fetch('EMPLEADO')
+    {
+      position_id:          user_data['ID_PUESTO'],
+      position_name:        user_data['DENOMINACION_PUESTO'],
+      functional_unit_id:   user_data['ID_UNIDAD_FUNCIONAL'],
+      functional_unit_name: user_data['DEN_UNIDAD_FUNCIONAL'],
+      org_code:             user_data['COD_ORGANICO'],
+      category:             user_data['CATEGORIA'],
+      category_name:        user_data['DEN_CATEGORIA'],
+      level:                user_data['NIVEL'],
+      personal_area:        user_data['AREA_PERSONAL'],
+      personal_area_name:   user_data['DEN_AREA_PERSONAL'],
+    }
+  end
+
   def parser
     @parser ||= Nori.new
   end
