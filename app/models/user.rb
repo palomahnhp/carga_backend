04dcad15b21d48e8ec4301c06095973059640048
Daniel Nacenta <dnacenta2@gmail.com>
2017-06-08 12:23:31 +0200

    Add response associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :responses
   belongs_to :position
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170607075012) do
+ActiveRecord::Schema.define(version: 20170608101103) do
 
   create_table "campaigns", force: :cascade do |t|
     t.string   "name"
@@ -42,6 +42,11 @@ ActiveRecord::Schema.define(version: 20170607075012) do
   end
 
   add_index "positions", ["unit_id"], name: "index_positions_on_unit_id"
+
+  create_table "responses", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "surveys", force: :cascade do |t|
     t.datetime "created_at", null: false
