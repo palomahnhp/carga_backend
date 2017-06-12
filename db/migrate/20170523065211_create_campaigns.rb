97835cc7132e94cd340fa6b654af4964e54bb8dc
Daniel Nacenta <dnacenta2@gmail.com>
2017-06-12 09:57:05 +0200

    Add campaign status

--- db/migrate/20170523065211_create_campaigns.rb
+++ db/migrate/20170523065211_create_campaigns.rb
@@ -3,6 +3,7 @@ class CreateCampaigns < ActiveRecord::Migration
     create_table :campaigns do |t|
       t.string :name
       t.integer :response_level
+      t.column :status, :integer, default: 0
 
       t.timestamps null: false
     end
