288255cfd1569f6170b66aa03e6aaf0a0746d3e0
Daniel Nacenta <dnacenta2@gmail.com>
2017-06-22 09:07:55 +0200

    WIP group mailing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ApplicationMailer
 
   def reminder_email(mail_address, options = {})
     @message = options[:message]
-    mail(from: options[:sender].presence || Rails.application.secrets.default_email_sender, to: mail_address, subject: options[:subject])
+    mail(from: options[:sender].presence || Rails.application.secrets.default_email_sender, to: mail_address, bcc: bcc_mail_addresses, subject: options[:subject])
   end
 end
