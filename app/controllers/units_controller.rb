52b7f7f558599362c2c636f75c7846fab41dfb83
Daniel Nacenta <dnacenta2@gmail.com>
2017-06-30 12:14:15 +0200

    Fix unit settings

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -1,13 +1,33 @@
 class UnitsController < ApplicationController
   def index
     @units = Unit.all
+    if params[:search]
+      @units = Unit.search(params[:search]).order('id DESC')
+    else
+      @units = Unit.all.order('id DESC')
+    end
+  end
+
+  def show
+    @unit = Unit.find(params[:id])
+  end
+
+  def edit
+    @unit = Unit.find(params[:id])
   end
 
   def create
     @unit = Unit.create(
       unit_number: params[:unit_number],
       name:        params[:name],
-      campaign_id: params[:campaign_id]
+      campaign_id: params[:campaign_id],
+      alias:       params[:alias],
+      cod_area:    params[:cod_area],
+      area_name:   params[:area_name],
+      cod_dir:     params[:cod_dir],
+      dir_name:    params[:dir_name],
+      cod_subdir:  params[:campaign_id],
+      subdir_name: params[:dir_name]
       )
     if @unit.save
       puts 'unit saved'
@@ -19,8 +39,15 @@ class UnitsController < ApplicationController
     @unit = Unit.find(params[:id])
     @unit.update_attributes(
       unit_number: params[:unit_number],
-      name:        params[:name], 
-      campaign_id: params[:campaign_id]
+      name:        params[:name],
+      campaign_id: params[:campaign_id],
+      alias:       params[:alias],
+      cod_area:    params[:cod_area],
+      area_name:   params[:area_name],
+      cod_dir:     params[:cod_dir],
+      dir_name:    params[:dir_name],
+      cod_subdir:  params[:cod_subdir],
+      subdir_name: params[:subdir_name]
       )
     redirect_to action: :index
   end
@@ -44,6 +71,6 @@ class UnitsController < ApplicationController
   private
 
   def unit_params
-  params.require(:unit).permit(:unit_number, :name, :campaign_id)
+  params.require(:unit).permit(:unit_number, :name, :campaign_id, :alias, :cod_area, :area_name, :cod_dir, :dir_name, :cod_subdir, :subdir_name)
   end
 end
