893f48d3a0c3854bc9b13e9ffa43d874f87d5d9c
Daniel Nacenta <dnacenta2@gmail.com>
2017-07-25 10:17:36 +0200

    Fix responses validations

--- app/models/response.rb
+++ app/models/response.rb
@@ -3,8 +3,8 @@ class Response < ActiveRecord::Base
   belongs_to :function
 
   validates :time_per, presence: {message: "Falta porcentaje de tiempo de la tarea"}
-  validates :num_task, presence: {message: "Falta número de tareas"}
-  validates :min_time, presence: {message: "Falta tiempo mínimo de la tarea"}
-  validates :avg_time, presence: {message: "Falta tiempo medio de la tarea"}
-  validates :max_time, presence: {message: "Falta tiempo medio de la tarea"}
+  # validates :num_task, presence: {message: "Falta número de tareas"}
+  # validates :min_time, presence: {message: "Falta tiempo mínimo de la tarea"}
+  # validates :avg_time, presence: {message: "Falta tiempo medio de la tarea"}
+  # validates :max_time, presence: {message: "Falta tiempo medio de la tarea"}
 end
