6574666939fb25da5151ba9a0ea6f6efb9f0d5f7
Daniel Nacenta <dnacenta2@gmail.com>
2017-08-08 12:57:15 +0200

    Add Directorio functionality

--- app/controllers/concerns/login_manager.rb
+++ app/controllers/concerns/login_manager.rb
@@ -25,6 +25,36 @@ class LoginManager
         )
         copy_errors_from!(user) unless user.save
       end
+
+      data = employees_data(pern: nil, nif: login_data[:document])
+      dir_data = data['EMPLEADOS_ACTIVOS']['EMPLEADO']
+      puts "****************************************"
+      puts dir_data['NIF']
+      puts "****************************************"
+
+      unless user.user_dir != nil
+        ActiveRecord::Base.transaction do
+          UserDir.create(
+            pernr:               dir_data['PERNR'],
+            nif:                 dir_data['NIF'],
+            ayre:                dir_data['AYRE'],
+            last_name:           dir_data['APELLIDO1'],
+            last_name_alt:       dir_data['APELLIDO2'],
+            name:                dir_data['NOMBRE'],
+            position_id:         dir_data['ID_PUESTO'],
+            position_des:        dir_data['DENOMINACION_PUESTO'],
+            functional_unit_id:  dir_data['ID_UNIDAD_FUNCIONAL'],
+            functional_unit_des: dir_data['DEN_UNIDAD_FUNCIONAL'],
+            organic_code:        dir_data['COD_ORGANICO'],
+            category:            dir_data['CATEGORIA'],
+            category_den:        dir_data['DEN_CATEGORIA'],
+            level:               dir_data['NIVEL'],
+            personal_area:       dir_data['AREA_PERSONAL'],
+            personal_area_den:   dir_data['DEN_AREA_PERSONAL'],
+            email:               dir_data['EMAIL'],
+          )
+        end
+      end
     end
     user
   end
@@ -48,4 +78,29 @@ class LoginManager
   def required_data_present?
     %i(login uweb_id email document).all? { |key| login_data.key? key }
   end
+
+  def request(operation, message_params)
+    response = d_client.call(operation.to_sym, message: message_params).body
+    d_parsed_response = d_parser.parse(response[(operation.to_s + '_response').to_sym][(operation.to_s + '_return').to_sym])
+    return d_parsed_response
+  rescue Savon::Error => e
+    puts "#{operation} - #{message_params} - Error Savon: #{e}"
+    false
+  end
+
+  def employees_data(params)
+    params[:i_tipoConsulta] = {only_unit: 1, all_down: 2}[params[:i_tipoConsulta]] if params[:i_tipoConsulta].present?
+    message_params = {aplicacion: Rails.application.secrets.directorio_application_key}
+    message_params.merge!(params)
+    request(:datos_empleados, message_params)
+  end
+
+  def d_client
+    @d_client ||= Savon.client(wsdl: Rails.application.secrets.directorio_wsdl, raise_errors: true)
+  end
+
+  def d_parser
+    @parser ||= Nori.new
+  end
+  ######
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_one :user_dir
   has_many :responses
   belongs_to :position
 
