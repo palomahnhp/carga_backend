e6c2e776d1a7fecca5c7ec0dd1b3b0451784a00e
Javi Colomo <sacjavicolomo3@hotmail.com>
2017-08-21 12:47:56 +0200

    Fixed CSV exportation

--- app/models/function.rb
+++ app/models/function.rb
@@ -10,7 +10,8 @@ class Function < ActiveRecord::Base
     CSV.generate(col_sep:';', encoding:'ISO-8859-1') do |csv|
       csv << ["Función", "Puesto"]
       records.each do |record|
-        csv << [record.name.gsub!('\u2019', '\''), record.position.name]
+        name = record.name.include?('’') ? record.name.gsub!('’', '\'') : record.name
+        csv << [name, record.position.name]
       end
     end
   end
