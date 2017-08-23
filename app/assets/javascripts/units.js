53c564e988b047810205647a9f39162ec2982417
Javi Colomo <sacjavicolomo3@hotmail.com>
2017-08-23 11:41:00 +0200

    Added orgchart for units

---
+++ app/assets/javascripts/units.js
@@ -0,0 +1,74 @@
+function filterElement(edit) {
+  if ($("#area option:selected").val() == "") {
+    $("#direction").empty();
+    $("#subdirection").empty();
+  } else {
+    switch(edit) {
+      case "direction":
+        area = $("#area option:selected").val();
+        dataHash = {
+          edit: edit,
+          area: area
+        }
+        break;
+      case "subdirection":
+        area = $("#area option:selected").val();
+        dir = $("#direction option:selected").val();
+        dataHash = {
+          edit: edit,
+          area: area,
+          dir: dir
+        }
+        break;
+    }
+    $.ajax({
+      url: "/units",
+      dataType: 'json',
+      data: dataHash,
+      success: function (data) {
+        $("#"+edit).empty();
+        data.forEach(function(element) {
+          name = "";          
+          switch(edit) {
+            case "direction":
+              name = element.dir_name;
+              break;
+            case "subdirection":
+              name = element.subdir_name;
+              break;
+          }
+          $("#"+edit).append("<option value='" + name + "'>" + name + "</option>");
+          $("#"+edit).trigger("change");
+        });
+        return {results: data};
+      }
+    });
+  }
+}
+
+function filterOrgchart(area, dir) {
+  dataHash = {
+    op: "initEdit",
+    area: area,
+    dir: dir
+  }
+  $.ajax({
+      url: "/units",
+      dataType: 'json',
+      data: dataHash,
+      async: false,
+      success: function (data) {
+        $("#direction").empty();
+        $("#subdirection").empty();
+        data.dir.forEach(function(element) {
+          name = element.dir_name;
+          $("#direction").append("<option value='" + name + "'>" + name + "</option>");
+        });
+        data.subdir.forEach(function(element) {
+          name = element.subdir_name;
+          $("#subdirection").append("<option value='" + name + "'>" + name + "</option>");
+        });
+        return {results: data};
+      }
+  });
+}
