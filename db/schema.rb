ad7f910f199615939fca5a1c8d7604c2854b077a
Javi Colomo <sacjavicolomo3@hotmail.com>
2017-09-06 09:38:38 +0200

    Added unique index to positions_number

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807093354) do
+ActiveRecord::Schema.define(version: 20170906073455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,19 @@ ActiveRecord::Schema.define(version: 20170807093354) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
 
   create_table "functions", force: :cascade do |t|
     t.integer  "position_id"
@@ -39,8 +52,11 @@ ActiveRecord::Schema.define(version: 20170807093354) do
     t.string   "position_number"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "slug"
   end
 
+  add_index "positions", ["position_number"], name: "index_positions_on_position_number", unique: true, using: :btree
+  add_index "positions", ["slug"], name: "index_positions_on_slug", unique: true, using: :btree
   add_index "positions", ["unit_id"], name: "index_positions_on_unit_id", using: :btree
 
   create_table "responses", force: :cascade do |t|
