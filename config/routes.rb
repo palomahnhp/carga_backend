b8b22535335b6d67c299bf0baabf41459ccaa21d
Javi Colomo <sacjavicolomo3@hotmail.com>
2017-09-14 12:18:12 +0200

    Fixed survey instructions and files

--- config/routes.rb
+++ config/routes.rb
@@ -45,6 +45,7 @@ Rails.application.routes.draw do
   get '/surveys/show', to: 'surveys#show'
   get '/surveys/reset_responses', to: 'surveys#reset_responses'
   get '/surveys/download_calc', to: 'surveys#download_calc'
+  get '/surveys/download_manual', to: 'surveys#download_manual'
 
   get '/responses', to: 'responses#index'
   get '/responses/show', to: 'responses#show'
