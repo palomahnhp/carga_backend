7efbdecca4dad8c18a85e3b8a47e917615adc203
jcolomos <sacjavicolomo3@hotmail.com>
2017-09-15 10:16:58 +0200

    Add any_answer column into units for trackings filtering

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170907070806) do
+ActiveRecord::Schema.define(version: 20170914115456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,8 +85,9 @@ ActiveRecord::Schema.define(version: 20170907070806) do
     t.string   "dir_name"
     t.string   "cod_subdir"
     t.string   "subdir_name"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "any_answer",  default: false
   end
 
   add_index "units", ["campaign_id"], name: "index_units_on_campaign_id", using: :btree
