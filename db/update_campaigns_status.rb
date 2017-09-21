6c91cc0073d6d5f8a8e24d5f0c78edf16da23c82
Javi Colomo <sacjavicolomo3@hotmail.com>
2017-09-21 10:10:28 +0200

    Updated campaigns task

--- db/update_campaigns_status.rb
+++ db/update_campaigns_status.rb
@@ -1,9 +1,14 @@
-puts "--- ACTUALIZANDO ESTADO DE LAS CAMPAÑAS ---"
+puts "\n\n--- ACTUALIZANDO ESTADO DE LAS CAMPAÑAS ---"
 
+updated = 0
 Campaign.all.each do |campaign|
-  if Time.now.to_date > campaign.end_date.to_date
-    campaign.update_attributes!(status: Campaign.statuses[:completed])
+  if (Time.now.to_date > campaign.end_date.to_date) && campaign.status != Campaign.statuses.key(2)
+    campaign.update_attributes(status: Campaign.statuses[:completed])
+    campaign.save(validate: false)
+    puts "----- Campaña \"#{campaign.name}\" finalizada, fecha pasada de finalización: #{campaign.end_date.to_date} -----"
+    updated += 1
   end
 end
 
+puts "--- CAMPAÑAS ACTUALIZADAS: #{updated}   ---"
 puts "--- ACTUALIZACIÓN COMPLETADA            ---"
