041653195522e975a75cdc34535c0d3aadabb208
Javi Colomo <sacjavicolomo3@hotmail.com>
2017-10-09 11:11:26 +0200

    Fixed outter dependencies and position bug in surveys

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,13 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
+Rails.application.config.assets.precompile += %w( jquery-ui.css )
+Rails.application.config.assets.precompile += %w( ajax-core.js )
+Rails.application.config.assets.precompile += %w( html5shiv.js )
+Rails.application.config.assets.precompile += %w( respond.js )
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( jquery-ui.js )
 Rails.application.config.assets.precompile += %w( datepicker-es.js )
 
 # Precompile additional assets.
