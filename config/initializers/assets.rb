5749e63b14a90ee776940194c0ef7833d706d412
Javi Colomo <sacjavicolomo3@hotmail.com>
2017-10-23 13:05:41 +0200

    Assets initializer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 
 Rails.application.config.assets.precompile += %w( jquery-ui.css )
+Rails.application.config.assets.precompile += %w( sweetalert2.min.css )
 Rails.application.config.assets.precompile += %w( ajax-core.js )
 Rails.application.config.assets.precompile += %w( html5shiv.js )
 Rails.application.config.assets.precompile += %w( respond.js )
@@ -14,6 +15,13 @@ Rails.application.config.assets.precompile += %w( jquery.min.js )
 Rails.application.config.assets.precompile += %w( bootstrap.min.js )
 Rails.application.config.assets.precompile += %w( jquery-ui.js )
 Rails.application.config.assets.precompile += %w( datepicker-es.js )
+Rails.application.config.assets.precompile += %w( campaigns.js )
+Rails.application.config.assets.precompile += %w( positions.js )
+Rails.application.config.assets.precompile += %w( functions.js )
+Rails.application.config.assets.precompile += %w( surveys.js )
+Rails.application.config.assets.precompile += %w( units.js )
+Rails.application.config.assets.precompile += %w( users.js )
+Rails.application.config.assets.precompile += %w( sweetalert2.min.js )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
